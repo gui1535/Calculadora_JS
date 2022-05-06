288a6d9fc6caac5b3c00d6c8bfb6bb89b990d06e
gui1535 <guilherme.araujo1535@gmail.com>
2022-05-06 20:21:59 -0300

    Inicio

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
-function calcular(tipo, valor){
-      
+function calcular(tipo, valor) {
+
+    if (tipo == 'acao') {
+
+        if (valor === 'c') {
+            // Limpar o visor
+            document.getElementById('resultado').value = '';
+        }
+
+        if (valor === '+' || valor === '-' || valor === '*' || valor === '/' || valor === '.') {
+            document.getElementById('resultado').value += valor;
+        }
+
+        if (valor === '=') {
+            var valor_campo = document.getElementById('resultado').value;
+            console.log(valor_campo);
+        }
+    }
+    else if (tipo == 'valor') {
+        document.getElementById('resultado').value += valor; // Capturando o valor dentro do ID resultado
+    }
+
 }
